1a8366745cea1a68b0ed0ca4a6cbb6d917147fb0
Fredric Dorothy <fredricdorothy@gmail.com>
2019-10-06 06:30:19 -0500

    added basic demo map

---
+++ Assets/Maps/ground.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="ground" tilewidth="16" tileheight="16" tilecount="96" columns="16">
+ <image source="../Images/tileset.png" width="256" height="96"/>
+ <terraintypes>
+  <terrain name="grass" tile="10"/>
+  <terrain name="island" tile="35"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,0"/>
+ <tile id="1" terrain="1,1,0,0"/>
+ <tile id="2" terrain="1,1,0,1"/>
+ <tile id="3" terrain="0,0,0,1"/>
+ <tile id="4" terrain="0,0,1,0"/>
+ <tile id="16" terrain="1,0,1,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,1,0,1"/>
+ <tile id="19" terrain="0,1,0,0"/>
+ <tile id="20" terrain="1,0,0,0"/>
+ <tile id="32" terrain="1,0,1,1"/>
+ <tile id="33" terrain="0,0,1,1"/>
+ <tile id="34" terrain="0,1,1,1"/>
+ <tile id="35" terrain="1,1,1,1"/>
+ <tile id="75" terrain=",0,,"/>
+</tileset>
---
+++ Assets/Maps/plants.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="plants" tilewidth="12" tileheight="24" tilecount="78" columns="78">
+ <image source="../Images/plants.png" width="936" height="24"/>
+</tileset>
